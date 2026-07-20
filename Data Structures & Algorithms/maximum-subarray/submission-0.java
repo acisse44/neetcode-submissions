8d394b0dc73945b8640576a96b18f981c4fe8184
Aminata Cisse <125103142+acisse44@users.noreply.github.com>
2026-07-19 22:33:46 -0400

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        //Kadane's Algorithm problem
+
+        // at every point we ask, do we want to continue with the prev. elements
+        // or drop them as they are hurting us (harmful load)
+
+        int prevSum =0;
+        int max =nums[0] ;
+
+        for(int i =0; i<nums.length; i++) {
+        
+        int currentNum = nums[i];
+
+            if((prevSum + currentNum) < currentNum) {
+                prevSum = currentNum;
+            }else{
+                prevSum = prevSum + currentNum;
+            }
+
+            max = Math.max(max, prevSum);
+        }
+        
+            return max;
+
+    }
+     
+}
